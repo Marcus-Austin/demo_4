0c66b9313d885b4958ffe0f2b30bb2d50c350dd2
Mirella Irene Artischeff <milla.artischeff@gmail.com>
2025-12-01 19:39:52 -0300

    first commit

---
+++ src/main/java/com/example/demo_for_4/DemoFor4Application.java
@@ -0,0 +1,13 @@
+package com.example.demo_for_4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoFor4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoFor4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo_for_4/DemoFor4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_for_4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoFor4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
